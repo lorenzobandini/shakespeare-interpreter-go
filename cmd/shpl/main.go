a555dd5ae466a42b88f0712de697b448cdcb2e3e
lorenzo <lboa@hotmail.it>
2026-07-09 18:25:42 +0200

    feat(cli): add tokens subcommand for lexer output

--- cmd/shpl/main.go
+++ cmd/shpl/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/lorenzobandini/shakespeare-interpreter-go/internal/lexer"
+	"github.com/lorenzobandini/shakespeare-interpreter-go/internal/logger"
+)
+
+var debugFlag bool
+
+var rootCmd = &cobra.Command{
+	Use:   "shpl",
+	Short: "Shakespeare Programming Language interpreter",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if debugFlag {
+			logger.Init(logger.LevelDebug)
+		} else {
+			logger.Init(logger.LevelInfo)
+		}
+	},
+}
+
+var tokensCmd = &cobra.Command{
+	Use:   "tokens <file>",
+	Short: "Tokenize an SPL source file and print the token stream",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		src, err := os.ReadFile(args[0])
+		if err != nil {
+			return fmt.Errorf("cannot read file %q: %w", args[0], err)
+		}
+		tokens, err := lexer.New(string(src)).ScanTokens()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		out := cmd.OutOrStdout()
+		for _, tok := range tokens {
+			if _, err := fmt.Fprintln(out, tok.String()); err != nil {
+				return err
+			}
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVar(&debugFlag, "debug", false, "enable debug logging")
+	rootCmd.AddCommand(tokensCmd)
+}
 
 func main() {
-	fmt.Println("Shakespeare Language Interpreter")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
--- cmd/shpl/main_test.go
+++ cmd/shpl/main_test.go
@@ -1,11 +1,34 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
 
 func TestMainEngineSetup(t *testing.T) {
 	expectedStatus := true
 
 	if !expectedStatus {
 		t.Errorf("Setup validation failed: expected %t, got false", expectedStatus)
+	}
+}
+
+func TestTokensCommand(t *testing.T) {
+	buf := &bytes.Buffer{}
+	rootCmd.SetOut(buf)
+	rootCmd.SetArgs([]string{"tokens", "../../testdata/lexer/minimal.shpl"})
+	defer rootCmd.SetArgs([]string{})
+	defer rootCmd.SetOut(nil)
+
+	if err := rootCmd.Execute(); err != nil {
+		t.Fatal(err)
+	}
+	output := buf.String()
+	if !strings.Contains(output, "{WORD") {
+		t.Errorf("output missing WORD tokens:\n%s", output)
+	}
+	if !strings.Contains(output, "{EOF") {
+		t.Errorf("output missing EOF token:\n%s", output)
 	}
 }
